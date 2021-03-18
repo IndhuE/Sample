fa9f0588cfc9edb0e8ed71b8a6ef0307400983c6
IndhuE <indhuvip30@gmail.com>
2021-03-18 23:39:06 +0530

    pushing sample folder

---
+++ src/org/acc/test/login/A.java
@@ -0,0 +1,57 @@
+package org.acc.test.login;
+public class A extends B{
+	public static void simple1() {
+		int a= 10;
+	}
+	public static void main(String[] args) {
+		simple1();
+		simple();
+	}
+}
+
+
+
+//static in variable level
+//public class A extends B{
+//		static int b = 20;
+//	
+//	public static void main(String[] args) {
+//		System.out.println("b is"+b);
+//		System.out.println(a);
+//	}
+//}
+//	static int a=10;
+//	int b=10;
+//	private void company() {
+//		System.out.println("a"+a);
+//		System.out.println("b"+b);
+//		a++;
+//		b++;
+//	}
+//	public static void main(String[] args) {
+//		A x=new A();
+//		A x1=new A();
+//		A x3=new A();
+//		System.out.println("static "+a);
+//		System.out.println("instance "+x.b);
+//		x.company();
+//		x1.company();
+//		x3.company();
+//	}
+//	public static void main(String[] args) {
+//		StringBuffer s = new StringBuffer("java");
+//		String s1=new String("java");
+//		String s2=new String("sql");
+//		StringBuffer s3=s1.append(s2);
+//		System.out.println(System.identityHashCode(s));
+//		System.out.println(System.identityHashCode(s1));
+//		System.out.println(System.identityHashCode(s2));
+//		System.out.println(System.identityHashCode(s3));
+//
+	//}
+	
+	
+		
+		
+		
+
---
+++ src/org/acc/test/login/B.java
@@ -0,0 +1,13 @@
+package org.acc.test.login;
+
+public class B {
+	public static void simple() {
+		int b= 20;
+		
+	}
+}
+
+//static in variable level
+//public class B {
+//		 static int a = 10;	  
+//}
